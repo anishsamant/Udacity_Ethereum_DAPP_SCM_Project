b85bf021e943e907fd1bc747716c70c873be7500
Anish Samant <anish.samant97@gmail.com>
2023-12-03 18:35:29 -0500

    built access control contracts

--- contracts/coffeebase/SupplyChain.sol
+++ contracts/coffeebase/SupplyChain.sol
@@ -1,9 +1,14 @@
 // SPDX-License-Identifier: MIT
+import "../coffeeaccesscontrol/ConsumerRole.sol";
+import "../coffeeaccesscontrol/DistributorRole.sol";
+import "../coffeeaccesscontrol/FarmerRole.sol";
+import "../coffeeaccesscontrol/RetailerRole.sol";
 
 pragma solidity ^0.8.0;
 
 // Define a contract 'Supplychain'
-contract SupplyChain {
+contract SupplyChain is ConsumerRole, DistributorRole, FarmerRole, RetailerRole
+ {
 
   // Define 'owner'
   address owner;
